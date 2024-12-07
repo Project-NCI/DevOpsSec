52520f33121682571c651ba601c6a4ebee525d3c
Om Khulbe <omkhulbe3@gmail.com>
2024-12-07 19:53:40 +0000

    Initial Commit

---
+++ DublinEvent/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def eventregistration(request):
+    return render(request, 'event.html')
